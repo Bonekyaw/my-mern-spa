6b46d594893508007c256bebfdc68167a04dac38
Phone Nyo <phonenai2014@gmail.com>
2020-07-10 17:28:51 +0630

    Register, Login & auth token middleware success

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const config = require('./config/key');
 const {User} = require('./models/user');
+const {auth} = require('./middleware/auth');
 
 mongoose.connect(config.mongoURI,{useNewUrlParser:true,useUnifiedTopology: true})
             .then(() => console.log("DB Connected"))
@@ -14,15 +15,54 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use(cookieParser());
 
+app.get('/api/users/auth', auth, (req, res) => {
+    res.status(200).json({
+        _id: req._id,
+        isAuth: true,
+        email: req.user.email,
+        name: req.user.name,
+        lastname: req.user.lastname,
+        role: req.user.role
+    })
+});
+
 app.post('/api/users/register', (req, res) => {
     const user = new User(req.body);
     user.save((err,doc) => {
         if(err) return res.json({success:false,err});
+        return res.status(200).json({
+            success:true,
+            userData: doc
+        });
     });
-    return res.status(200).json({success:true});
 });
 
+app.post('/api/users/login', (req, res) => {
+    //Find email matched
+   User.findOne({email: req.body.email}, (err, user) => {
+       if (!user) 
+           return res.json({
+               loginSuccess: true,
+               message: "Auth failed, email not found"
+           });
+           //Compare password Hash
+           user.comparePassword(req.body.password, (err, isMatch) => {
+               if (!isMatch) {
+                   return res.json({loginSuccess: false, message: "wrong password"})
+               } 
+           })
+           //generate token
+           user.generateToken((err, user) => {
+               if(err) return res.status(400).send(err);
+               res.cookie('x_auth', user.token)
+                            .status(200)
+                            .json({
+                                loginSuccess: true
+                            })
+           })
 
+   }) 
+});
 
 app.listen(5000, () => {
     console.log('App listening on port 5000!');
