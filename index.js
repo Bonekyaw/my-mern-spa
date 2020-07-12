57e4990657498d84aafea6bb8ed989626846958b
Phone Nyo <phonenai2014@gmail.com>
2020-07-12 16:30:23 +0630

    Err Solution again

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const {auth} = require('./middleware/auth');
 // Load config
 dotenv.config({ path: './config/config.env' });
 
-mongoose.connect(config.mongoURI,{
+mongoose.connect(process.env.MONGO_URI,{
                 useNewUrlParser:true,
                 useUnifiedTopology: true,
                 useCreateIndex: true,
